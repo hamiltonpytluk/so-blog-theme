20204f015d4a0088de1ff74fc80c10f2bb43b785
Hamilton Pytluk <hamiltonpytluk@gmail.com>
2015-04-06 10:26:44 -0400

    Initial

---
+++ footer.php
@@ -0,0 +1,72 @@
+		 		</div><!--//.center-->
+		 		<div class="col right">
+		 			<div class="newsletter-signup">
+		 				<h3>Join Our Email List</h3>
+		 				<form action="http://bigsea.createsend.com/t/y/s/diidjk/" method="post">
+		 					<span>Name:</span>
+		 					<input type="text" name="cm-name" id="name" />
+		 					<span>Email:</span>
+		 					<input type="text" name="cm-diidjk-diidjk" id="diidjk-diidjk" />
+		 					<div class="button">
+		 						<span>Sign Up</span>
+		 						<input type="submit" value="Sign Up" />
+		 					</div>
+		 				</form>
+		 			</div><!--//.newsletter-signup-->
+		 			<div class="block">
+		 				<h3>Videos</h3>
+		 				<ul class="videos">
+		 					<li><a href="http://www.youtube.com/watch?v=cXyFBSSmDH4&feature=youtu.be"><img src="/wp-content/themes/starlightorchestra/img/video.gif" alt="Video"> Starlight Music</a></li>
+		 					<li><a href="http://www.youtube.com/watch?v=VLxUr59UJTo&feature=player_embedded"><img src="/wp-content/themes/starlightorchestra/img/video.gif" alt="Video"> Additional Products &amp; Services</a></li>
+		 					<li><a href="http://vimeo.com/7349211"><img src="/wp-content/themes/starlightorchestra/img/video.gif" alt="Video"> Starlight Orchestra's String Ensemble</a></li>
+		 					<li><a href="http://www.starlightorchestrasblog.com/bandleaders/the-starlight-experience/"><img src="/wp-content/themes/starlightorchestra/img/video.gif" alt="Video"> Starlight Experience</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="twitter block">
+		 				<h3>Twitter</h3>
+		 				<a class="twitter-timeline" href="https://twitter.com/StarlightOrch" data-widget-id="435162413923450880" data-tweet-limit="1"  data-chrome="noheader nofooter noborders transparent" data-link-color="#401575" >Tweets by @StarlightOrch</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+		 			</div>
+		 			<div class="block">
+		 				<h3>Instagram</h3>
+		 				<div id="instafeed" class="clearfix"></div>
+		 			</div>
+		 			<div class="block">
+		 				<ul class="images">
+		 					<li><a href="http://nymag.com/weddings/planner/2009/summer/trade-off/"><img src="/wp-content/themes/starlightorchestra/img/nymag.gif" alt="As Featured In New York Weddings" /></a></li>
+		 					<li><a href="http://www.marthastewartweddings.com/search?vgnextoid=d3862f09fba1d110VgnVCM1000003d370a0aRCRD&rsc=search_home&autonomy_kw=romanoff"><img src="/wp-content/themes/starlightorchestra/img/stewart.gif" alt="As Featured In Marth Stewart weddings" /></a></li>
+		 					<li><a href="http://thebridalcircle.com/friday-buzz-interview-with-valerie-romanoff-starlight-orchestras/"><img src="/wp-content/themes/starlightorchestra/img/bridalcircle.jpg" alt="As Featured In The Bridal Circle" /></a></li>
+		 				</ul>
+		 			</div>
+		 		</div><!--//.right-->
+		 	</div><!--//#content-->
+	 	
+	 	</div><!--//.all-->
+ 
+	 	<div id="footer" class="wrap clearfix">
+	 		<div class="left">
+	 			<ul class="clearfix">
+	 				<?php // wp_nav_menu( array('menu' => 'Footer Menu' )); ?>
+	 			</ul>
+	 			<span>&copy;2014 Starlight Entertainment. All Rights Reserved.</span>
+	 		</div><!--//.roll-->
+	 		<div class="right">
+	 			<ul class="social">
+		 			<li class="facebook"><a href="https://www.facebook.com/starlightorchestras"><span>Facebook</span></a></li>
+		 			<li class="twitter"><a href="https://twitter.com/starlightorch"><span>Twitter</span></a></li>
+		 			<li class="instagram"><a href="http://instagram.com/starlightorchestras"><span>Instagram</span></a></li>
+		 			<li class="youtube"><a href="http://www.youtube.com/StarlightOrchestras"><span>YouTube</span></a></li>
+		 		</ul>
+	 		</div>
+	 	</div><!--//#footer-->
+	 	
+	 	<script type="text/javascript" src="//use.typekit.net/yeq1aqe.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+		<script src="/wp-content/themes/starlightorchestra/js/app.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+
+	 </body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Starlight blog</title>
+		<style type="text/css">
+			@import "/wp-content/themes/starlightorchestra/style.css"; 
+		</style>
+		<meta name="viewport" content="width=device-width">
+	</head>
+	<body>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=217354711792394";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	 
+	 	<div class="all">
+	 
+		 	<div id="header" class="wrap">
+			 	<div class="clearfix">
+			 		<div class="back-to">
+			 			<a href="#">&lsaquo; Back To Main Site</a>
+			 		</div>
+			 		<ul class="social">
+			 			<li class="facebook"><a href="https://www.facebook.com/starlightorchestras"><span>Facebook</span></a></li>
+			 			<li class="twitter"><a href="https://twitter.com/starlightorch"><span>Twitter</span></a></li>
+			 			<li class="instagram"><a href="http://instagram.com/starlightorchestras"><span>Instagram</span></a></li>
+			 			<li class="youtube"><a href="http://www.youtube.com/StarlightOrchestras"><span>YouTube</span></a></li>
+			 		</ul>
+			 	</div>
+			 	<div class="mast">
+				 	<h1><a href="/"><span>Starlight Orchestras</span></a></h1>
+			 	</div>
+		 	</div><!--//#header-->
+	 
+		 	<div id="content" class="wrap clearfix">
+		 		<div class="col left">
+		 			<?php get_sidebar('Sidebar'); ?>		 			
+		 		</div><!--//.left-->
+		 		<div class="col center">
